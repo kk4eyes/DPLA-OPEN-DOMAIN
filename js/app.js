e15e3bae29824824959a12ea010e6eb599e7b019
kkindorf <kevin.kindorf@gmail.com>
2016-06-10 15:51:12 -0400

    added functionality for twitter button, download button

--- js/app.js
+++ js/app.js
@@ -7,6 +7,30 @@ var featherEditor = new Aviary.Feather({
     onSave: function(imageID, newURL) {
         var img = document.getElementById(imageID);
         img.src = newURL;
+
+
+        if($("#"+imageID).parents(".pix:first").find(".downloadButton").length > 0 ){
+          $("#"+imageID).parents(".pix:first").find(".downloadButton").attr({"href":newURL, "download":newURL});
+        }   
+        else{
+          var downloadButton= "<a class='downloadButton' download='"+newURL+"'href='"+newURL+"'title='ImageName'>Download</a>";
+          $("#"+imageID).parents(".pix:first").append(downloadButton);
+        }   
+       
+        var tweetButton = $("#"+imageID).parents(".pix:first").find(".tweetButton");
+        //console.log(tweetButton);
+        var tweetHref = tweetButton.attr("href");
+        console.log(tweetHref);
+
+        var editedURL = tweetHref.split("dpla:")[0]+"dpla:"+newURL+"&hashtags"+tweetHref.split("&hashtags")[1];
+
+        console.log(editedURL);
+
+        tweetButton.attr("href", editedURL);
+
+
+
+
     },
     onError: function(errorObj) {
         alert(errorObj.message);
@@ -39,24 +63,21 @@ $(function() {
             console.log(data);
             var image = data.docs;
             $.each(image, function(i, value) {
-              
-
                 var link = data.docs[i].isShownAt;
                 var dataProvider = data.docs[i].dataProvider;
                  if (searchTerm.indexOf(" ")>0){
                   subject = searchTerm.replace(" ", "");
                 }
                 else{
-
                 subject = searchTerm;
               }
               console.log(subject);
                 console.log(link);
                
-                var tweetButton = "<a href='https://twitter.com/intent/tweet?text=No copyright photo via the " + dataProvider + " and dpla:" + link + "&hashtags=" + subject + ", dpla'>Tweet This</a>";
+                var tweetButton = "<a class='tweetButton' href='https://twitter.com/intent/tweet?text=No copyright photo via the " + dataProvider + " and dpla:" + link + "&hashtags=" + subject + ", dpla'>Tweet This</a>";
                 console.log(subject);
                
-                $("#injection_site").append("<div class='pix'><img id='image" + i + "'src=" + data.docs[i].object + "><button id='button" + i + "'>Edit</button>" + tweetButton + "</div>")
+                $("#injection_site").append("<div class='pix'><a href="+link+"><img id='image" + i + "'src=" + data.docs[i].object + "></a><button id='button" + i + "'>Edit</button>" + tweetButton + "</div>")
                 $("#button" + i).click(function() {
                     launchEditor("image" + i, data.docs[i].object);
 
